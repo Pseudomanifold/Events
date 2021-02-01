069bd458d53e190706b576a4497f1684670201c4
Timothy Gibbons <tjgibbons10@gmail.com>
2021-02-01 05:43:44 -0600

    Event Type
    Modified Events and Event Dispatcher to allow for enum event types.
    This is basically a rewrite, I tried to keep it as true to the original blog post.
    My changes could be outlined from https://github.com/Pseudomanifold/Events/issues/1.
    These changes made Event and Event Dispatcher into templated classes. These templated classes will accept a `enum` as it's type.
    This move the event type to the compiler for validation.

--- DemoEvent.hh
+++ DemoEvent.hh
@@ -3,15 +3,31 @@
 
 #include "Event.hh"
 
-class DemoEvent : public Event
+enum class EventType1 {
+  TEST_EVENT,
+  TEST_EVENT2,
+  TEST_EVENT3
+};
+
+enum class EventType2 {
+  TEST_EVENT,
+  TEST_EVENT2,
+  TEST_EVENT3
+};
+
+class DemoEvent : public Event<EventType1>
 {
 public:
-  DemoEvent();
-  virtual ~DemoEvent();
+  DemoEvent() : Event<EventType1>(EventType1::TEST_EVENT, "DemoEvent"){};
+  virtual ~DemoEvent() = default;
+};
 
-  static constexpr DescriptorType descriptor = "DemoEvent";
-
-  virtual DescriptorType type() const;
+class DemoEventAlt : public Event<EventType2>
+{
+private:
+public:
+  DemoEventAlt() : Event<EventType2>(EventType2::TEST_EVENT, "DemoEventAlt"){};
+  virtual ~DemoEventAlt() = default;
 };
 
 #endif
